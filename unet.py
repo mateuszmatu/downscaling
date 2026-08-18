46ac62da10f620cb694a01dfd667b411ceb9aac9
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-08-18 15:01:35 +0200

    group normalization for stability

--- unet.py
+++ unet.py
@@ -32,10 +32,10 @@ class DoubleConv(nn.Module):
         super(DoubleConv, self).__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.GroupNorm(num_groups=8, num_channels=out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.GroupNorm(num_groups=8, num_channels=out_channels),
             nn.ReLU(inplace=True)
         )
 
